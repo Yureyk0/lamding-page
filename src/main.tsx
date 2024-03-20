8fe1d5fb25c57689bffac3fe92d850939ab94e5a
Yura Boreyko <yuraboreyko@gmail.com>
2024-03-20 19:36:22 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Home } from "./Pages/Home/Home";
 
-import "./index.css";
 import "./assets/fonts/fonts.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
