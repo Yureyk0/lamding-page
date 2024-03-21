d4b30ad1a79891a00187f6f133578999f4487c65
Yura Boreyko <yuraboreyko@gmail.com>
2024-03-21 10:44:19 +0200

    hotfix video

--- src/Pages/Home/componetns/Video/Popup/Popup.tsx
+++ src/Pages/Home/componetns/Video/Popup/Popup.tsx
@@ -1,6 +1,7 @@
 import ReactPlayer from "react-player";
 
 import "./Popup.css";
+import { useScreenSize } from "../../../../../hooks/useScreenSize";
 
 interface PopupProps {
   videoUrl: string;
@@ -8,13 +9,23 @@ interface PopupProps {
 }
 
 export const Popup = ({ videoUrl, onClose }: PopupProps) => {
+  const { width } = useScreenSize();
+
+  const isModile = width < 500;
+  console.log(isModile);
+
   return (
     <div className="popup-container">
       <div className="popup-inner">
         <button className="close-btn" onClick={onClose}>
           &#10006;
         </button>
-        <ReactPlayer url={videoUrl} controls={true} />
+        <ReactPlayer
+          url={videoUrl}
+          controls={true}
+          width={isModile ? 333 : 640}
+          height={isModile ? 200 : 360}
+        />
       </div>
     </div>
   );
